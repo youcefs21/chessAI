5f79814865b373d3b30111c2368f7034bfe8a662
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2024-12-04 14:00:29 -0500

    finishing preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 piece_indices = {
-    'P': 1, 'N': 2, 'B': 3, 'R': 4, 'Q': 5, 'K': 6,
-    'p': 7, 'n': 8, 'b': 9, 'r': 10, 'q': 11, 'k': 12
+    "P": 1,
+    "N": 2,
+    "B": 3,
+    "R": 4,
+    "Q": 5,
+    "K": 6,
+    "p": 7,
+    "n": 8,
+    "b": 9,
+    "r": 10,
+    "q": 11,
+    "k": 12,
 }
+
 
 def preprocess_board(fen):
     """
@@ -12,10 +23,10 @@ def preprocess_board(fen):
     There are 17 planes:
     Piece Planes (0-11): Each plane corresponds to one type of piece (e.g., white pawn, black rook).
     """
-    pieces = fen.split(' ')[0]
-    rows = pieces.split('/')
+    pieces = fen.split(" ")[0]
+    rows = pieces.split("/")
     board = np.zeros((8, 8, 12), dtype=np.float32)
-    
+
     # Populate piece planes
     for i, row in enumerate(rows):
         j = 0
@@ -25,7 +36,7 @@ def preprocess_board(fen):
                     j += 1
             elif char.isalpha():
                 piece_index = piece_indices[char]
-                board[i, j, piece_index-1] = 1
+                board[i, j, piece_index - 1] = 1
                 j += 1
-    
+
     return board
